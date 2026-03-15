535ece983305ac351204528085134db368efa92e
magiclz233 <magiclz233@163.com>
2026-03-15 20:17:26 +0800

    refactor: 移除不必要的fmt导入和错误打印
    删除main.go中未使用的fmt包导入，并移除已由cli.Execute处理的错误信息打印，简化代码结构。

--- cmd/syskit/main.go
+++ cmd/syskit/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"syskit/internal/cli"
 	"syskit/internal/errs"
@@ -10,7 +9,6 @@ import (
 
 func main() {
 	if err := cli.Execute(version.Value); err != nil {
-		fmt.Fprintf(os.Stderr, "错误: %v\n", err)
 		os.Exit(errs.Code(err))
 	}
 }
